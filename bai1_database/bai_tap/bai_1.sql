87ecca21b29a211bbdc60fd96188da596abfdb48
manhhung1509 <hung2332nguyenmanh@gmail.com>
2021-12-09 13:57:20 +0700

    upbai

---
+++ bai1_database/bai_tap/bai_1.sql
@@ -0,0 +1,28 @@
+create database student_management;
+
+use student_management;
+
+create table class(
+id int primary key,
+`name` varchar(50),
+gmail varchar(50) 
+);
+
+insert into class
+values (1,'hung','hung@gamil.com');
+
+insert into class
+values (2,'hoang','hoang@gmail.com');
+
+alter table class
+add height int;
+
+insert into class
+values(4,'nha','nha@gmail.com',60),
+(5,'thanh','thanh@gmail.com',65);
+
+update class
+set height = 5
+where id = 1 or id = 2;
+
+SELECT * FROM class;
